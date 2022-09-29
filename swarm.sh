a5c997e97870a800280f976027e63d471ecd306f
Lumiere-Lodi <lumiere@symbionix.co>
2022-09-29 18:07:26 +0200

    Remove hapi fhir config importer

--- swarm.sh
+++ swarm.sh
@@ -47,6 +47,7 @@ main() {
     log info "Waiting to give config importers time to run before cleaning up service"
     config::remove_config_importer cares-clickhouse-config-importer
     config::remove_config_importer cares-superset-config-importer
+    config::remove_config_importer hapi-fhir-config-importer
 
     # Ensure config importer is removed
     config::await_service_removed instant_cares-clickhouse-config-importer
