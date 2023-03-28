763f56f4b164a65f301459fca83da7fa2094a353
Arran Standish <arranstandish@hotmail.com>
2023-03-28 08:32:52 +0200

    Update cares to parameterise the stack names for each importer that it needs to run against, to be able to find the right service

--- swarm.sh
+++ swarm.sh
@@ -5,6 +5,7 @@ declare COMPOSE_FILE_PATH=""
 declare UTILS_PATH=""
 declare CONFIG_LABELS=()
 declare SERVICE_IMPORTER_NAMES=()
+declare STACK="cares"
 
 function init_vars() {
   ACTION=$1
@@ -34,6 +35,7 @@ function init_vars() {
   readonly CONFIG_LABELS
   readonly UTILS_PATH
   readonly SERVICE_IMPORTER_NAMES
+  readonly STACK
 }
 
 # shellcheck disable=SC1091
@@ -67,14 +69,15 @@ function clean_containers_and_configs() {
 }
 
 function restart_hapi_fhir() {
-  if docker service ps -q instant_hapi-fhir &>/dev/null; then
+  local -r stackname="${FHIR_STACKNAME:-hapi-fhir}"
+  if docker service ps -q ${stackname}_hapi-fhir &>/dev/null; then
     log info "Restarting HAPI FHIR..."
     try \
-      "docker service scale instant_hapi-fhir=0" \
+      "docker service scale ${stackname}_hapi-fhir=0" \
       throw \
       "Error scaling down hapi-fhir to update the IG"
     try \
-      "docker service scale instant_hapi-fhir=$HAPI_FHIR_INSTANCES" \
+      "docker service scale ${stackname}_hapi-fhir=$HAPI_FHIR_INSTANCES" \
       throw \
       "Error scaling up hapi-fhir to update the IG"
   else
@@ -91,8 +94,8 @@ function clean_stale_config_importers() {
       continue
     fi
 
-    config::remove_config_importer "$service_name"
-    config::await_service_removed instant_"$service_name"
+    config::remove_config_importer $STACK "$service_name"
+    config::await_service_removed ${STACK}_"$service_name"
   done
 
   log info "Removing stale configs..."
@@ -112,20 +115,22 @@ function initialize_package() {
   config::generate_service_configs cares-clickhouse-config-importer / "${COMPOSE_FILE_PATH}/importer/analytics-datastore-clickhouse/" "${COMPOSE_FILE_PATH}/importer" clickhouse
   clickhouse_temp_compose_param="-c ${COMPOSE_FILE_PATH}/importer/docker-compose.tmp.yml"
 
-  try "docker stack deploy -c ${COMPOSE_FILE_PATH}/importer/docker-compose.config.yml $clickhouse_temp_compose_param instant" throw "Failed to deploy Cares on Platform"
+  try "docker stack deploy -c ${COMPOSE_FILE_PATH}/importer/docker-compose.config.yml $clickhouse_temp_compose_param $STACK" throw "Failed to deploy Cares on Platform"
 
   log info "Waiting to update configs"
   # Kafka Mapper Consumer
+  local -r kafka_stackname="${KAFKA_STACKNAME:-kafka}"
   REF_service_update_args=""
   config::update_service_configs REF_service_update_args /app/src/data "$COMPOSE_FILE_PATH"/importer/kafka-mapper-consumer/mapping kafka-mapper-consumer
   config::update_service_configs REF_service_update_args /app/src/plugin "$COMPOSE_FILE_PATH"/importer/kafka-mapper-consumer/plugin kafka-mapper-consumer
-  try "docker service update $REF_service_update_args instant_kafka-mapper-consumer" throw "Failed to update config for instant_kafka-mapper-consumer"
+  try "docker service update $REF_service_update_args ${kafka_stackname}_kafka-mapper-consumer" throw "Failed to update config for ${kafka_stackname}_kafka-mapper-consumer"
   # Superset
+  local -r superset_stackname="${SUPERSET_STACKNAME:-superset}"
   REF_service_update_args=""
   config::update_service_configs REF_service_update_args /app/pythonpath "$COMPOSE_FILE_PATH"/importer/dashboard-visualiser-superset superset
   # TODO: Update .env.superset once the value for MAPBOX_API_KEY is known
   config::env_var_add_from_file REF_service_update_args "$COMPOSE_FILE_PATH"/.env.superset
-  try "docker service update $REF_service_update_args instant_dashboard-visualiser-superset" throw "Failed to update config for instant_dashboard-visualiser-superset"
+  try "docker service update $REF_service_update_args ${superset_stackname}_dashboard-visualiser-superset" throw "Failed to update config for ${superset_stackname}_dashboard-visualiser-superset"
 
   clean_stale_config_importers
 
@@ -153,7 +158,7 @@ main() {
   elif [[ "${ACTION}" == "destroy" ]]; then
     log info "Destroying package"
 
-    docker::service_destroy "${SERVICE_IMPORTER_NAMES[@]}"
+    docker::stack_destroy $STACK
   else
     log error "Valid options are: init, up, down, or destroy"
   fi
